484cdc018fe8812acebe0318be19ee8badaba16f
Chethan1306 <166906416+Chethan1306@users.noreply.github.com>
2024-05-31 18:58:58 +0530

    Added parameters newly

--- MainDemo/src/test/java/package1/ContactTest.java
+++ MainDemo/src/test/java/package1/ContactTest.java
@@ -6,6 +6,14 @@ public class ContactTest {
     @Test(groups="Smoke Test")
     public  void createContactTest()
     {
+    	String URL = System.getProperty("Url");
+		String BROWSER = System.getProperty("Browser");
+		String USERNAME = System.getProperty("Username");
+		String PASSWORD = System.getProperty("Password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
     	System.out.println("Execute create contact");
     }
     @Test
--- MainDemo/src/test/java/package2/OrgTest.java
+++ MainDemo/src/test/java/package2/OrgTest.java
@@ -6,6 +6,14 @@ public class OrgTest {
 	@Test(groups="Smoke Test")
 	public void createOrgTest()
 	{
+		String URL = System.getProperty("Url");
+		String BROWSER = System.getProperty("Browser");
+		String USERNAME = System.getProperty("Username");
+		String PASSWORD = System.getProperty("Password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 		System.out.println("Execute create org");
 	}
 	@Test(groups="Regression Test")
